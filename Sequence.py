196abb39889c6d733970d9e48d49160bde00ed72
Dennis Ritter <dennis.r.ritter@gmail.com>
2019-07-12 14:59:59 +0200

    added pca calculation to Sequence class

--- Sequence.py
+++ Sequence.py
@@ -1,4 +1,5 @@
 import numpy
+from sklearn.decomposition import PCA
 
 
 class Sequence:
@@ -17,6 +18,7 @@ class Sequence:
         #             [[part-i+1.x, part-i+1.y, part-i+1.z], [part-i+1.x, part-i+1.y, part-i+1.z], [part-i+1.x, part-i+1.y, part-i+1.z]],
         #             ...
         #          ]
+        # shape: (num_body_parts, num_keypoints, xyz)
         self.positions = numpy.array(positions)
         # Timestamps for when the positions have been tracked
         # Example: [<someTimestamp1>, <someTimestamp2>, <someTimestamp3>, ...]
@@ -26,3 +28,19 @@ class Sequence:
         # Example: [["Head", "Neck"], ["Neck", "RShoulder"], ["RShoulder", "RElbow"], ["RElbow", "RWrist"], ...],
         self.body_pairs = body_pairs
         """
+        self.positions_2d = self.get_positions_2d()
+
+    def get_positions_2d(self):
+        """
+        Returns the positions for all keypoints in 
+        shape: (num_keypoints, num_bodyparts * xyz).
+        """
+        return numpy.ndarray.flatten(self.positions).reshape(self.positions.shape[1], -1)
+
+    def get_pcs(self, num_components: int = 3):
+        """
+        Calculates n principal components for the tracked positions of this sequence
+        """
+        pca = PCA(n_components=num_components)
+        xPCA = pca.fit_transform(self.get_positions_2d())
+        return xPCA
