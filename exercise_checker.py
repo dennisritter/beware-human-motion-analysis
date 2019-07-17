8daf168acbabc7099d1681cf968207fb4c2d6816
DennisRitter <dennis.r.ritter@gmail.com>
2019-07-17 17:18:18 +0200

    added todos

--- exercise_checker.py
+++ exercise_checker.py
@@ -3,3 +3,10 @@ from exercise_loader import load
 
 ex = load('data/exercises/squat.json')
 print(ex.__dict__)
+
+# TODO: Analyse motion sequence angles
+# -> Define Angles:Human_Joints mapping to validate correct angles
+# -> Get motion sequence
+# -> Get groups of necessary joints for angle validation
+# -> Get angle for all(?) frames of the sequence
+# -> Check if angle breaks defined rules for angles in exercise
