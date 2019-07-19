97544044acd4ab0de686889e682f6663e952b663
DennisRitter <dennis.r.ritter@gmail.com>
2019-07-19 17:38:34 +0200

    added module file for medical angle calculations

---
+++ src/angle_calculations_medical.py
@@ -0,0 +1,18 @@
+from Sequence import Sequence
+
+
+def calc_angles_lefthip_flexion_extension(seq: Sequence, joints: dict) -> list:
+    """ Calculates the hips flexion/extension angles for each frame of the Sequence.
+    Parameters
+    ----------
+    seq : Sequence
+        A Motion Sequence
+    joints : dict
+        The joints to use for angle calculation.
+        Attributes:
+            angle_vertex : int
+            rays : list<int>
+        Example: { "angle_vertex": 1, "rays": [0, 2] }
+    """
+    print("brum brum")
+    return []
