88d7f0529d4ed659d3bdd2cd824949d35558c6df
DennisRitter <dennis.r.ritter@gmail.com>
2019-07-19 17:40:13 +0200

    extended start script

--- src/exercise_checker.py
+++ src/exercise_checker.py
@@ -2,17 +2,27 @@ from Exercise import Exercise
 from JointsAngleMapper import JointsAngleMapper
 from PoseFormatEnum import PoseFormatEnum
 from exercise_loader import load
+from Sequence import Sequence
+from PoseMapper import PoseMapper
+import angle_calculations_medical as acm
 
 # Get Exercise Object from json file
 ex = load('data/exercises/squat.json')
 
+# Get PoseMapper instance for MOCAP sequences
+mocap_posemapper = PoseMapper(PoseFormatEnum.MOCAP)
+# Convert mocap json string Positions to Sequence Object
+seq = mocap_posemapper.load('data/sequences/squat_3/complete-session.json', 'Squat')
+
+# Add joints to angles property of exercise
 jam = JointsAngleMapper(PoseFormatEnum.MOCAP)
 jam.addJointsToAngles(ex)
 
-print(ex.angles)
+# Get
+joints = jam.jointsMap
+# Calculate angles for Sequence
+acm.calc_angles_lefthip_flexion_extension(seq, joints["hip_left"]["flexion_extension"])
+
 # TODO: Analyse motion sequence angles
-# -> Define Angles:Human_Joints mapping to validate correct angles
-# -> Get motion sequence
-# -> Get groups of necessary joints for angle validation
 # -> Get angle for all(?) frames of the sequence
 # -> Check if angle breaks defined rules for angles in exercise
