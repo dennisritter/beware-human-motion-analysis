c8d0ebe5c04dbe8bc0da2a394662bd124d52733b
DennisRitter <dennis.r.ritter@gmail.com>
2019-07-22 18:05:35 +0200

    impolemented knee flexion extension angle calc

--- src/angle_calculations_medical.py
+++ src/angle_calculations_medical.py
@@ -31,3 +31,34 @@ def calc_angle_hip_flexion_extension(seq: Sequence, joints: dict) -> list:
 
     print(angles)
     return angles
+
+
+def calc_angle_knee_flexion_extension(seq: Sequence, joints: dict) -> list:
+    # NOTE: Observations and Potential Problems:
+    #       * Torso position might be incorrect for heavy people with big upper body
+    #           => Possible solution: Calibration to calculate a bias? [Tell user to Stand (start) -> Track pose -> calculate bias]
+    """ Calculates the Knees flexion/extension angles for each frame of the Sequence.
+    Parameters
+    ----------
+    seq : Sequence
+        A Motion Sequence
+    joints : dict
+        The joints to use for angle calculation.
+        Attributes:
+            angle_vertex : int
+            rays : list<int>
+        Example: { "angle_vertex": 1, "rays": [0, 2] }
+    """
+    knee = seq.positions[:, joints["angle_vertex"], :]
+    hip = seq.positions[:, joints["rays"][0], :]
+    ankle = seq.positions[:, joints["rays"][1], :]
+    angles = []
+    for i in range(len(hip)):
+        knee_hip = hip[i] - knee[i]
+        knee_ankle = ankle[i] - knee[i]
+        cos_angle = np.dot(knee_hip, knee_ankle) / (np.linalg.norm(knee_hip) * np.linalg.norm(knee_ankle))
+        angle = np.arccos(cos_angle)
+        angles.append(180 - np.degrees(angle))
+
+    print(angles)
+    return angles
--- src/exercise_checker.py
+++ src/exercise_checker.py
@@ -13,17 +13,22 @@ ex = exercise_loader.load('data/exercises/squat.json')
 # Get PoseMapper instance for MOCAP sequences
 mocap_posemapper = PoseMapper(PoseFormatEnum.MOCAP)
 # Convert mocap json string Positions to Sequence Object
-seq = mocap_posemapper.load('data/sequences/squat_4/complete-session.json', 'Squat')
+seq = mocap_posemapper.load('data/sequences/squat_3/complete-session.json', 'Squat')
 
 # Add joints to angles property of exercise
 jam = JointsAngleMapper(PoseFormatEnum.MOCAP)
 jam.addJointsToAngles(ex)
 
 joints = jam.jointsMap
+
 # Calculate angles for Sequence
 # Left Hip Flexion/Extension
 acm.calc_angle_hip_flexion_extension(seq, joints["hip_left"]["flexion_extension"])
 # Right Hip Flexion/Extension
 acm.calc_angle_hip_flexion_extension(seq, joints["hip_right"]["flexion_extension"])
+# Left Knee Flexion/Extension
+acm.calc_angle_knee_flexion_extension(seq, joints["knee_left"]["flexion_extension"])
+# Right Knee Flexion/Extension
+acm.calc_angle_knee_flexion_extension(seq, joints["knee_right"]["flexion_extension"])
 # Visualize angle
-visualize.vis_angle(seq, joints["hip_left"]["flexion_extension"], 0)
+visualize.vis_angle(seq, joints["knee_left"]["flexion_extension"], 50)
