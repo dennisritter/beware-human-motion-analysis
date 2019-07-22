88cc385224e744f853111c9485568ee4b7c3fcb5
DennisRitter <dennis.r.ritter@gmail.com>
2019-07-22 15:38:02 +0200

    visualizing pose in 2d

--- src/visualize.py
+++ src/visualize.py
@@ -21,8 +21,8 @@ def vis_angle_for_frame(seq: Sequence, angle: dict, frame: int):
     angle_ray_b = angle["rays"][1]
 
     fig = plt.figure(figsize=(10, 10), dpi=80)
-    ax = plt.axes(projection='3d')
-    # ax.set_aspect('equal')
+    ax = plt.axes()
+    ax.set_aspect('equal')
     # x,y,z for angle vertices
     # angle_x = [seq.positions[frame][angle_vertex][0], seq.positions[frame][angle_ray_a][0], seq.positions[frame][angle_ray_b][0]]
     # ignore x values
@@ -30,14 +30,22 @@ def vis_angle_for_frame(seq: Sequence, angle: dict, frame: int):
     angle_y = [seq.positions[frame][angle_vertex][1], seq.positions[frame][angle_ray_a][1], seq.positions[frame][angle_ray_b][1]]
     angle_z = [seq.positions[frame][angle_vertex][2], seq.positions[frame][angle_ray_a][2], seq.positions[frame][angle_ray_b][2]]
 
-    ax.scatter(angle_x, angle_y, angle_z, c='r')
-    ax.plot([angle_x[0], angle_x[1]], [angle_y[0], angle_y[1]], [angle_z[0], angle_z[1]], color='r')
-    ax.plot([angle_x[0], angle_x[2]], [angle_y[0], angle_y[2]], [angle_z[0], angle_z[2]], color='r')
-    # ax.plot([seq.positions[frame][angle_vertex][0], seq.positions[frame][angle_ray_a][0]], [seq.positions[frame][angle_vertex][1], seq.positions[frame][angle_ray_a][1]], [seq.positions[frame][angle_vertex][2], seq.positions[frame][angle_ray_a][2]], color='r')
-    # ax.plot([seq.positions[frame][angle_vertex][0], seq.positions[frame][angle_ray_b][0]], [seq.positions[frame][angle_vertex][1], seq.positions[frame][angle_ray_b][1]], [seq.positions[frame][angle_vertex][2], seq.positions[frame][angle_ray_b][2]], color='r')
-    ax.text(angle_x[0], angle_y[0], angle_z[0], inv_body_parts[angle_vertex], color="r")
-    ax.text(angle_x[1], angle_y[1], angle_z[1], inv_body_parts[angle_ray_a], color="r")
-    ax.text(angle_x[2], angle_y[2], angle_z[2], inv_body_parts[angle_ray_b], color="r")
+    # 3d
+    # ax.scatter(angle_x, angle_y, angle_z, c='r')
+    # ax.plot([angle_x[0], angle_x[1]], [angle_y[0], angle_y[1]], [angle_z[0], angle_z[1]], color='r')
+    # ax.plot([angle_x[0], angle_x[2]], [angle_y[0], angle_y[2]], [angle_z[0], angle_z[2]], color='r')
+
+    # ax.text(angle_x[0], angle_y[0], angle_z[0], inv_body_parts[angle_vertex], color="r")
+    # ax.text(angle_x[1], angle_y[1], angle_z[1], inv_body_parts[angle_ray_a], color="r")
+    # ax.text(angle_x[2], angle_y[2], angle_z[2], inv_body_parts[angle_ray_b], color="r")
+    # 2d
+    ax.scatter(angle_z, angle_y, c='r')
+    ax.plot([angle_z[0], angle_z[1]], [angle_y[0], angle_y[1]], color='r')
+    ax.plot([angle_z[0], angle_z[2]], [angle_y[0], angle_y[2]], color='r')
+
+    ax.text(angle_z[0], angle_y[0], inv_body_parts[angle_vertex], color="r")
+    ax.text(angle_z[1], angle_y[1], inv_body_parts[angle_ray_a], color="r")
+    ax.text(angle_z[2], angle_y[2], inv_body_parts[angle_ray_b], color="r")
 
     noangle_x = []
     noangle_y = []
@@ -49,9 +57,13 @@ def vis_angle_for_frame(seq: Sequence, angle: dict, frame: int):
         # noangle_x.append(seq.positions[frame][i][0])
         noangle_y.append(seq.positions[frame][i][1])
         noangle_z.append(seq.positions[frame][i][2])
-        ax.text(seq.positions[frame][i][0], seq.positions[frame][i][1], seq.positions[frame][i][2], inv_body_parts[i], color="blue")
+        # ax.text(seq.positions[frame][i][0], seq.positions[frame][i][1], seq.positions[frame][i][2], inv_body_parts[i], color="blue")
+        ax.text(seq.positions[frame][i][2], seq.positions[frame][i][1], inv_body_parts[i], color="blue")
 
-    ax.scatter(noangle_x, noangle_y, noangle_z, color="blue")
+    # 2d
+    ax.scatter(noangle_z, noangle_y, color="blue")
+    # 3d
+    # ax.scatter(noangle_x, noangle_y, noangle_z, color="blue")
     plt.show()
 
     print(f"Position angle_vertex: {angle_x[0]},{angle_y[0]},{angle_z[0]}")
