ecbb3706ef9a5cb0d7670dcba3dfa3cf489601ba
Dennis Ritter <dennis.r.ritter@gmail.com>
2019-07-23 14:17:44 +0200

    added more angle calculations

--- src/angle_calculations_medical.py
+++ src/angle_calculations_medical.py
@@ -1,15 +1,15 @@
 from Sequence import Sequence
 import numpy as np
 
-def calc_angle_2d(angle_vertex_2d: list, ray_vertex_a: list, ray_vertex_b: list) -> float:
+def calc_angle(angle_vertex: list, ray_vertex_a: list, ray_vertex_b: list) -> float:
     """ Calculates the angle between angle_vertex_2d-ray_vertex_a and angle_vertex_2d-ray_vertex_b in 2D space.
     Returns
     ----------
     float
         Angle between angle_vertex_2d-ray_vertex_a and angle_vertex_2d-ray_vertex_b in degrees
     """
-    ray_a = ray_vertex_a - angle_vertex_2d
-    ray_b = ray_vertex_b - angle_vertex_2d
+    ray_a = ray_vertex_a - angle_vertex
+    ray_b = ray_vertex_b - angle_vertex
     cos_angle = np.dot(ray_a, ray_b) / (np.linalg.norm(ray_a) * np.linalg.norm(ray_b))
     return np.degrees(np.arccos(cos_angle))
 
@@ -34,13 +34,42 @@ def calc_angle_hip_flexion_extension(seq: Sequence, joints: dict) -> list:
     torso = seq.positions[:, joints["rays"][1], 1:]
     angles = []
     for i in range(len(hip)):
-        angles.append(180 - calc_angle_2d(hip[i], knee[i], torso[i]))
+        # Substract angle from 180 because 'Normal Standing' is defined as 0° 
+        angles.append(180 - calc_angle(hip[i], knee[i], torso[i]))
+
+    return angles
+
+
+def calc_angle_hip_abduction_adduction(seq: Sequence, joints: dict) -> list:
+    # NOTE: Observations and Potential Problems:
+    #   * 'Normal Standing' angle will never be 0° because using left/right shoulder-hip ray for calculation.
+    #       Possible solution:  -> Use a bias of 5-10°
+    #                           -> Don't use shoulder-hip ray but direct shoulder-floor ray for calculation
+    """ Calculates the hips abduction/adduction angles for each frame of the Sequence.
+    Parameters
+    ----------
+    seq : Sequence
+        A Motion Sequence
+    joints : dict
+        The joints to use for angle calculation.
+        Attributes:
+            angle_vertex : int
+            rays : list<int>
+        Example: { "angle_vertex": 1, "rays": [0, 2] }
+    """
+    # Ignore Z Axis for abduction/adduction
+    hip = seq.positions[:, joints["angle_vertex"], :2]
+    knee = seq.positions[:, joints["rays"][0], :2]
+    torso = seq.positions[:, joints["rays"][1], :2]
+    angles = []
+    for i in range(len(hip)):
+        # Substract angle from 180 because 'Normal Standing' is defined as 0° 
+        angles.append(180 - calc_angle(hip[i], knee[i], torso[i]))
 
     return angles
 
 
 def calc_angle_knee_flexion_extension(seq: Sequence, joints: dict) -> list:
-    # NOTE: Observations and Potential Problems:
     """ Calculates the Knees flexion/extension angles for each frame of the Sequence.
     Parameters
     ----------
@@ -58,6 +87,86 @@ def calc_angle_knee_flexion_extension(seq: Sequence, joints: dict) -> list:
     ankle = seq.positions[:, joints["rays"][1], :]
     angles = []
     for i in range(len(knee)):
-        angles.append(180 - calc_angle_2d(knee[i], hip[i], ankle[i]))
+        # Substract angle from 180 because 'Normal Standing' is defined as 0° 
+        angles.append(180 - calc_angle(knee[i], hip[i], ankle[i]))
 
     return angles
+
+
+def calc_angle_shoulder_flexion_extension(seq: Sequence, joints: dict) -> list:
+    """ Calculates the Shoulders flexion/extension angles for each frame of the Sequence.
+    Parameters
+    ----------
+    seq : Sequence
+        A Motion Sequence
+    joints : dict
+        The joints to use for angle calculation.
+        Attributes:
+            angle_vertex : int
+            rays : list<int>
+        Example: { "angle_vertex": 1, "rays": [0, 2] }
+    """
+    shoulder = seq.positions[:, joints["angle_vertex"], 1:]
+    elbow = seq.positions[:, joints["rays"][0], 1:]
+    hip = seq.positions[:, joints["rays"][1], 1:]
+    angles = []
+    for i in range(len(shoulder)):
+        # Substract angle from 180 because 'Normal Standing' (hanging arm) is defined as 0° 
+        angles.append(calc_angle(shoulder[i], elbow[i], hip[i]))
+
+    return angles
+
+
+def calc_angle_shoulder_abduction_adduction(seq: Sequence, joints: dict) -> list:
+    # NOTE: Observations and Potential Problems:
+    #   * 'Normal Standing' angle will never be 0° because using left/right shoulder-hip ray for calculation.
+    #       Possible solution:  -> Use a bias of 5-10°
+    #                           -> Don't use shoulder-hip ray but direct shoulder-floor ray for calculation
+    """ Calculates the hips abduction/adduction angles for each frame of the Sequence.
+    Parameters
+    ----------
+    seq : Sequence
+        A Motion Sequence
+    joints : dict
+        The joints to use for angle calculation.
+        Attributes:
+            angle_vertex : int
+            rays : list<int>
+        Example: { "angle_vertex": 1, "rays": [0, 2] }
+    """
+    # Ignore Z Axis for abduction/adduction
+    shoulder = seq.positions[:, joints["angle_vertex"], :2]
+    elbow = seq.positions[:, joints["rays"][0], :2]
+    hip = seq.positions[:, joints["rays"][1], :2]
+    angles = []
+    for i in range(len(shoulder)):
+        # Substract angle from 180 because 'Normal Standing' is defined as 0° 
+        angles.append(180 - calc_angle(shoulder[i], elbow[i], hip[i]))
+
+    return angles
+
+
+def calc_angle_elbow_flexion_extension(seq: Sequence, joints: dict) -> list:
+    """ Calculates the Elbows flexion/extension angles for each frame of the Sequence.
+    Parameters
+    ----------
+    seq : Sequence
+        A Motion Sequence
+    joints : dict
+        The joints to use for angle calculation.
+        Attributes:
+            angle_vertex : int
+            rays : list<int>
+        Example: { "angle_vertex": 1, "rays": [0, 2] }
+    """
+    elbow = seq.positions[:, joints["angle_vertex"], :]
+    wrist = seq.positions[:, joints["rays"][0], :]
+    shoulder = seq.positions[:, joints["rays"][1], :]
+    angles = []
+    for i in range(len(elbow)):
+        # Substract angle from 180 because 'Normal Standing' (straight arm) is defined as 0° 
+        angles.append(180 - calc_angle(elbow[i], wrist[i], shoulder[i]))
+
+    return angles
+
+
--- src/exercise_checker.py
+++ src/exercise_checker.py
@@ -23,13 +23,43 @@ joints = jam.jointsMap
 hip_left_flexion_extension_angles = acm.calc_angle_hip_flexion_extension(seq, joints["hip_left"]["flexion_extension"])
 # Right Hip Flexion/Extension
 hip_right_flexion_extension_angles = acm.calc_angle_hip_flexion_extension(seq, joints["hip_right"]["flexion_extension"])
+# Left Hip Abduction/Adduction
+hip_left_abduction_adduction_angles = acm.calc_angle_hip_abduction_adduction(seq, joints["hip_left"]["abduction_adduction"])
+# Right Hip Abduction/Adduction
+hip_right_abduction_adduction_angles = acm.calc_angle_hip_abduction_adduction(seq, joints["hip_right"]["abduction_adduction"])
+
 # Left Knee Flexion/Extension
 knee_left_flexion_extension_angles = acm.calc_angle_knee_flexion_extension(seq, joints["knee_left"]["flexion_extension"])
 # Right Knee Flexion/Extension
 knee_right_flexion_extension_angles = acm.calc_angle_knee_flexion_extension(seq, joints["knee_right"]["flexion_extension"])
 
+# Left Elbow Flexion/Extension
+shoulder_left_flexion_extension_angles = acm.calc_angle_shoulder_flexion_extension(seq, joints["shoulder_left"]["flexion_extension"])
+# Right Elbow Flexion/Extension
+shoulder_right_flexion_extension_angles = acm.calc_angle_shoulder_flexion_extension(seq, joints["shoulder_right"]["flexion_extension"])
+# Left Elbow Abduction/Adduction
+shoulder_left_abduction_adduction_angles = acm.calc_angle_shoulder_abduction_adduction(seq, joints["shoulder_left"]["abduction_adduction"])
+# Right Elbow Abduction/Adduction
+shoulder_right_abduction_adduction_angles = acm.calc_angle_shoulder_abduction_adduction(seq, joints["shoulder_right"]["abduction_adduction"])
+
+# Left Elbow Flexion/Extension
+elbow_left_flexion_extension_angles = acm.calc_angle_elbow_flexion_extension(seq, joints["elbow_left"]["flexion_extension"])
+# Right Elbow Flexion/Extension
+elbow_right_flexion_extension_angles = acm.calc_angle_elbow_flexion_extension(seq, joints["elbow_right"]["flexion_extension"])
 
 FRAME = 50
-print(knee_left_flexion_extension_angles[50])
+print(f"Hip Left Flexion/Extension angle [{FRAME}]: {hip_left_flexion_extension_angles[FRAME]}")
+print(f"Hip Right Flexion/Extension angle [{FRAME}]: {hip_right_flexion_extension_angles[FRAME]}")
+print(f"Hip Left Abduction/Adduction angle [{FRAME}]: {hip_left_abduction_adduction_angles[FRAME]}")
+print(f"Hip Right Abduction/Adduction angle [{FRAME}]: {hip_right_abduction_adduction_angles[FRAME]}")
+print(f"Knee Left Flexion/Extension angle [{FRAME}]: {knee_left_flexion_extension_angles[FRAME]}")
+print(f"Knee Right Flexion/Extension angle [{FRAME}]: {knee_right_flexion_extension_angles[FRAME]}")
+print(f"Shoulder Left Flexion/Extension angle [{FRAME}]: {shoulder_left_flexion_extension_angles[FRAME]}")
+print(f"Shoulder Right Flexion/Extension angle [{FRAME}]: {shoulder_right_flexion_extension_angles[FRAME]}")
+print(f"Shoulder Left Abduction/Adduction angle [{FRAME}]: {shoulder_left_abduction_adduction_angles[FRAME]}")
+print(f"Shoulder Right Abduction/Adduction angle [{FRAME}]: {shoulder_right_abduction_adduction_angles[FRAME]}")
+print(f"Elbow Left Flexion/Extension angle [{FRAME}]: {elbow_left_flexion_extension_angles[FRAME]}")
+print(f"Elbow Right Flexion/Extension angle [{FRAME}]: {elbow_right_flexion_extension_angles[FRAME]}")
+
 # Visualize angle
-visualize.vis_angle(seq, joints["knee_left"]["flexion_extension"], 50)
+visualize.vis_angle(seq, joints["shoulder_right"]["abduction_adduction"], FRAME)
