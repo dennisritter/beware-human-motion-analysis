f6baf247c8f497173040ab9b718bca235fbc49d0
DennisRitter <dennis.r.ritter@gmail.com>
2019-07-26 16:28:20 +0200

    rotating correctly

--- src/exercise_checker.py
+++ src/exercise_checker.py
@@ -10,6 +10,26 @@ from PoseMapper import PoseMapper
 import visualize
 import angle_calculations_medical as acm
 import numpy as np
+import math
+
+# Source: https://stackoverflow.com/questions/6802577/rotation-of-3d-vector
+
+
+def rotation_matrix(axis, theta):
+    """
+    Return the rotation matrix associated with counterclockwise rotation about
+    the given axis by theta radians.
+    """
+    axis = np.asarray(axis)
+    axis = axis / math.sqrt(np.dot(axis, axis))
+    a = math.cos(theta / 2.0)
+    b, c, d = -axis * math.sin(theta / 2.0)
+    aa, bb, cc, dd = a * a, b * b, c * c, d * d
+    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+    return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                     [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                     [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
+
 
 # Get Exercise Object from json file
 ex = exercise_loader.load('data/exercises/squat.json')
@@ -19,18 +39,14 @@ mocap_posemapper = PoseMapper(PoseFormatEnum.MOCAP)
 seq = mocap_posemapper.load('data/sequences/squat_3/complete-session.json', 'Squat')
 
 start_cs_origin = np.array([0, 0, 0])
-start_dir_x = np.array([1, 0, 0])
+start_dir_x = np.array([.5, 1, 0])
 start_dir_y = np.array([0, 1, 0])
 start_dir_z = np.array([0, 0, 1])
 target_cs_origin = np.array([1, 1, 1])
 target_dir_x = np.array([-1, 0, 0])
 target_dir_y = np.array([0, -1, 0])
 # find vector perpendicular to xy-plane
-target_dir_z = -np.cross(target_dir_x, target_dir_y)
-# translate
-target_dir_x = target_dir_x+target_cs_origin
-target_dir_y = target_dir_y+target_cs_origin
-target_dir_z = target_dir_z+target_cs_origin
+target_dir_z = np.cross(target_dir_x, target_dir_y)
 
 I = np.array([
     [1, 0, 0],
@@ -54,30 +70,34 @@ trans_dir_y = np.matmul(T, np.append(start_dir_y, 1))[:3]
 trans_dir_z = np.matmul(T, np.append(start_dir_z, 1))[:3]
 # Find X-rotation angle
 # Source http: // www.euclideanspace.com/maths/algebra/vectors/angleBetween/index.htm
-v1 = trans_dir_x - target_cs_origin
-v2 = target_dir_x - target_cs_origin
+v1 = (trans_dir_x - trans_cs_origin) / math.sqrt(np.dot(trans_dir_x - trans_cs_origin, trans_dir_x - trans_cs_origin))
+v2 = (target_dir_x - target_cs_origin) / math.sqrt(np.dot(target_dir_x - target_cs_origin, target_dir_x - target_cs_origin))
+v1_dot_v2 = np.dot(v1, v2)
+v1_cross_v2 = np.cross(v1, v2)
+# print(f"theta_cross v1: {np.degrees(np.arccos(np.dot(v1, v1_cross_v2)))}")
+# print(f"theta_cross v2: {np.degrees(np.arccos(np.dot(v2, v1_cross_v2)))}")
 # If theta 180° (dot product = -1 just switch directions
-if np.dot(v1, v2):
-    trans_dir_x = v1 * -1 + target_cs_origin
+if (v1_dot_v2 == -1):
+    print("v1_dot_v2 = 0")
+    v1 = v1 * -1 + target_cs_origin
 else:
-    theta = np.degrees(np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))))
-    trans_target_dir_rot_axis = np.cross(v1, v2) / np.linalg.norm(np.cross(v1, v2))
-    print(np.dot(v1, v2))
-
+    theta = np.arccos(v1_dot_v2)
+    print(f"Theta: {theta}")
     # Build Rotation Matrix for rotation about arbitrary axis (Rodrigues formula)
     # Source https: // en.wikipedia.org/wiki/Rodrigues % 27_rotation_formula
-    k = trans_target_dir_rot_axis
-    K = np.array([
-        [0, -k[2], k[1]],
-        [k[2], 0, -k[0]],
-        [-k[1], k[0], 0]
-    ])
-    # print(f"K: \n{K}")
-    # print(f"K.eig: \n{np.linalg.eig(K)}")
-    print(f"Theta: {theta}")
-    R0 = I + np.sin(theta)*K + (1-np.cos(theta))*K**2
-    # print(f"trans dir x: \n{R0}")
-    trans_dir_x = np.matmul(R0, trans_dir_x / np.linalg.norm(trans_dir_x))
+    axis = v1_cross_v2
+    R0 = rotation_matrix(axis, theta)
+    v1_new = np.dot(R0, v1)
+
+print(v1)
+print(v1_new)
+print(np.dot(v1, v1_new))
+print(np.degrees(np.arccos(np.dot(v1, v1_new))))
+
+v1 += target_cs_origin
+v1_new += target_cs_origin
+v2 += target_cs_origin
+v1_cross_v2 += target_cs_origin
 
 # TODO:
 # 1. Calc perpendicular vector for start_dir_x and target_dir_x
@@ -108,18 +128,26 @@ else:
 
 fig = plt.figure(figsize=plt.figaspect(1)*2)
 ax = fig.add_subplot(1, 1, 1, projection='3d')
-ax.scatter(start_cs_origin[0], start_cs_origin[1], start_cs_origin[2], c='blue')
-ax.scatter(target_cs_origin[0], target_cs_origin[1], target_cs_origin[2], c='red')
-ax.plot([start_cs_origin[0], start_dir_x[0]], [start_cs_origin[1], start_dir_x[1]], [start_cs_origin[2], start_dir_x[2]], color="black")
-ax.plot([start_cs_origin[0], start_dir_y[0]], [start_cs_origin[1], start_dir_y[1]], [start_cs_origin[2], start_dir_y[2]], color="blue")
-ax.plot([start_cs_origin[0], start_dir_z[0]], [start_cs_origin[1], start_dir_z[1]], [start_cs_origin[2], start_dir_z[2]], color="blue")
-ax.plot([target_cs_origin[0], target_dir_x[0]], [target_cs_origin[1], target_dir_x[1]], [target_cs_origin[2], target_dir_x[2]], color="pink")
-ax.plot([target_cs_origin[0], target_dir_y[0]], [target_cs_origin[1], target_dir_y[1]], [target_cs_origin[2], target_dir_y[2]], color="red")
-ax.plot([target_cs_origin[0], target_dir_z[0]], [target_cs_origin[1], target_dir_z[1]], [target_cs_origin[2], target_dir_z[2]], color="red")
-ax.plot([trans_cs_origin[0], trans_dir_x[0]], [trans_cs_origin[1], trans_dir_x[1]], [trans_cs_origin[2], trans_dir_x[2]], color="black")
-ax.plot([trans_cs_origin[0], trans_dir_y[0]], [trans_cs_origin[1], trans_dir_y[1]], [trans_cs_origin[2], trans_dir_y[2]], color="green")
-ax.plot([trans_cs_origin[0], trans_dir_z[0]], [trans_cs_origin[1], trans_dir_z[1]], [trans_cs_origin[2], trans_dir_z[2]], color="green")
-# ax.plot(target_cs_origin[0], target_dir_x, color="red")
+ax.set_xlim3d(-2, 2)
+ax.set_ylim3d(-2, 2)
+ax.set_zlim3d(-2, 2)
+# ax.scatter(start_cs_origin[0], start_cs_origin[1], start_cs_origin[2], c='blue')
+# ax.scatter(target_cs_origin[0], target_cs_origin[1], target_cs_origin[2], c='red')
+# ax.plot([start_cs_origin[0], start_dir_x[0]], [start_cs_origin[1], start_dir_x[1]], [start_cs_origin[2], start_dir_x[2]], color="black")
+# ax.plot([start_cs_origin[0], start_dir_y[0]], [start_cs_origin[1], start_dir_y[1]], [start_cs_origin[2], start_dir_y[2]], color="blue")
+# ax.plot([start_cs_origin[0], start_dir_z[0]], [start_cs_origin[1], start_dir_z[1]], [start_cs_origin[2], start_dir_z[2]], color="blue")
+# ax.plot([target_cs_origin[0], target_dir_x[0]], [target_cs_origin[1], target_dir_x[1]], [target_cs_origin[2], target_dir_x[2]], color="pink")
+# ax.plot([target_cs_origin[0], target_dir_y[0]], [target_cs_origin[1], target_dir_y[1]], [target_cs_origin[2], target_dir_y[2]], color="red")
+# ax.plot([target_cs_origin[0], target_dir_z[0]], [target_cs_origin[1], target_dir_z[1]], [target_cs_origin[2], target_dir_z[2]], color="red")
+# ax.plot([trans_cs_origin[0], trans_dir_x[0]], [trans_cs_origin[1], trans_dir_x[1]], [trans_cs_origin[2], trans_dir_x[2]], color="black")
+# ax.plot([trans_cs_origin[0], trans_dir_y[0]], [trans_cs_origin[1], trans_dir_y[1]], [trans_cs_origin[2], trans_dir_y[2]], color="green")
+# ax.plot([trans_cs_origin[0], trans_dir_z[0]], [trans_cs_origin[1], trans_dir_z[1]], [trans_cs_origin[2], trans_dir_z[2]], color="green")
+# ax.plot([trans_cs_origin[0], k[0]], [trans_cs_origin[1], k[1]], [trans_cs_origin[2], k[2]], color="red", linestyle="dotted")
+
+ax.plot([target_cs_origin[0], v1[0]], [target_cs_origin[1], v1[1]], [target_cs_origin[2], v1[2]], color="blue")
+ax.plot([target_cs_origin[0], v1_new[0]], [target_cs_origin[1], v1_new[1]], [target_cs_origin[2], v1_new[2]], color="green")
+ax.plot([target_cs_origin[0], v2[0]], [target_cs_origin[1], v2[1]], [target_cs_origin[2], v2[2]], color="red", alpha=.5)
+ax.plot([target_cs_origin[0], v1_cross_v2[0]], [target_cs_origin[1], v1_cross_v2[1]], [target_cs_origin[2], v1_cross_v2[2]], color="black", linestyle="dotted")
 plt.show()
 
 
