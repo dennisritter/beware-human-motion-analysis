a7a9d744fd74e1c5045d2f247a55f80ab0916d91
DennisRitter <dennis.r.ritter@gmail.com>
2019-08-02 15:34:17 +0200

    renamed starting script

--- src/check_angles.py
+++ src/check_angles.py
@@ -8,21 +8,6 @@ import numpy as np
 import transformations
 import math
 
-# Beispiel Schulter Winkelberechnung Schritte:
-# -> Koordinatensystem in Punkt verschieben (keypoint Schulter)
-# -> X-Achse bestimmen (Schulter-Schulter)
-# -> Z-Achse bestimmen (Schulter-Nacken-Schulter Dreieck Normale)
-# -> Y-Achse orthogonal zu XZ Ebene
-##
-# -> Transformationsmatrix bauen
-# -> Translation zu Schulter
-# -> Rotationen der Achsen
-##
-# -> Alle für den Winkel benötigten Punkte transformieren
-# -> Winkel in Kugelkoordinaten ausrechnen
-# -> Medizinische Winkel ableiten
-
-
 # Get Exercise Object from json file
 ex = exercise_loader.load('data/exercises/squat.json')
 # Get PoseMapper instance for MOCAP sequences
@@ -30,5 +15,5 @@ mocap_posemapper = PoseMapper(PoseFormatEnum.MOCAP)
 # Convert mocap json string Positions to Sequence Object
 seq = mocap_posemapper.load('data/sequences/squat_3/complete-session.json', 'Squat')
 
-acm.calc_angles_shoulder_left(seq, 2, 14, 3, 1, log=True)
+acm.calc_angles_shoulder_left(seq, 2, 14, 3, 1, 0, log=True)
 # acm.calc_angles_shoulder_right(seq, 14, 2, 3, 13, log=True)
