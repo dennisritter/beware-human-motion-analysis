038112ceda044dae4542ff389df75197e7b6bb68
DennisRitter <dennis.r.ritter@gmail.com>
2019-08-08 14:33:11 +0200

    minor changes to script

--- src/check_angles.py
+++ src/check_angles.py
@@ -41,10 +41,10 @@ left_shoulder_angles = acm.calc_angles_shoulder_left(seq, 2, 14, 3, 1, log=False
 right_shoulder_angles = acm.calc_angles_shoulder_right(seq, 14, 2, 3, 13, log=False)
 left_hip_angles = acm.calc_angles_hip_left(seq, 8, 11, 4, 7, log=False)
 right_hip_angles = acm.calc_angles_hip_right(seq, 11, 8, 4, 10, log=False)
-left_elbow_angle = acm.calc_angles_elbow(seq, 1, 2, 0)
-right_elbow_angle = acm.calc_angles_elbow(seq, 13, 14, 12)
-left_knee_angle = acm.calc_angles_knee(seq, 7, 8, 6)
-right_knee_angle = acm.calc_angles_knee(seq, 10, 11, 9)
+left_elbow_angles = acm.calc_angles_elbow(seq, 1, 2, 0)
+right_elbow_angles = acm.calc_angles_elbow(seq, 13, 14, 12)
+left_knee_angles = acm.calc_angles_knee(seq, 7, 8, 6)
+right_knee_angles = acm.calc_angles_knee(seq, 10, 11, 9)
 
 logging.log_angles(left_shoulder_angles,
                    right_shoulder_angles,
