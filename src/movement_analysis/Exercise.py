a34c0cd6104d0c60b9df37bdf1ce42acc3a0b3fc
DennisRitter <dennis.r.ritter@gmail.com>
2019-08-09 17:12:26 +0200

    Added angle state enums; First implementation of _check_angle_shoulder_left_flexion_extension

--- src/movement_analysis/Exercise.py
+++ src/movement_analysis/Exercise.py
@@ -1,15 +1,61 @@
 import warnings
+import math
+from movement_analysis.AngleTargetStates import AngleTargetStates
+from movement_analysis.AngleAnalysisResultStates import AngleAnalysisResultStates
 
 
 class Exercise:
 
-    def __init__(self, name: str, angles: dict, userId: int, description: str = "Sorry, there is no description"):
+    HIGH_PRIO = 1.0
+    MEDIUM_PRIO = 0.5
+    LOW_PRIO = 0.0
+
+    def __init__(self, name: str, angles: dict, userId: int = 0, description: str = "Sorry, there is no description"):
         # str - The name of this exercise
         self.name = name
         # dict - The angle restrictions for start/end state, for relevant bodyparts
         self.angles = angles
-        # int - The number of sets for this exercise
+        # int - The user id this exercise is specialized for
         self.userId = userId
+        # str - A description for this exercise
         self.description = description
 
+    def _check_angle_shoulder_left_flexion_extension(self, angle: list, target_state: AngleTargetStates, tolerance: int = 10) -> dict:
+        if target_state not in self.angles.keys():
+            warnings.warn("The target_state parameter value is not present in the Exercises' angles attribute. Cancelng Analysis.")
+            return []
+        
+        # Determine whether movement from START to END is Extension, Flexion, or None
+        target_end = self.angles[AngleTargetStates.END]["shoulder_left"]["flexion_extension"]["angle"][1]
+        target_start = self.angles[AngleTargetStates.START]["shoulder_left"]["flexion_extension"]["angle"][1]
+        target_end_is_flexion = target_end > target_start
+        target_end_is_extension = target_end < target_start
+        target_end_is_zero = target_end == target_start
+
+        target_min = min(self.angles[target_state]["shoulder_left"]["flexion_extension"]["angle"])
+        target_max = max(self.angles[target_state]["shoulder_left"]["flexion_extension"]["angle"])
+
+        result_state = AngleAnalysisResultStates.NONE
+        if target_state == AngleTargetStates.START:
+            if angle < target_min - tolerance:
+                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED if target_end_is_flexion else result_state
+                result_state = AngleAnalysisResultStates.TARGET_UNDERCUT if target_end_is_extension else result_state
+                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED if target_end_is_zero else result_state
+            if angle > target_max + tolerance:
+                result_state = AngleAnalysisResultStates.TARGET_UNDERCUT if target_end_is_flexion else result_state
+                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED if target_end_is_extension else result_state
+                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED if target_end_is_zero else result_state
+            if angle <= target_max + tolerance and angle >= target_min - tolerance:
+                result_state = AngleAnalysisResultStates.IN_TARGET_RANGE
+
+        result = {
+            "angle": angle,
+            "target_min": target_min,
+            "target_max": target_max,
+            "target_state": target_state,
+            "result_state": result_state,
+        }
+        return result
     
+    def _check_angles_shoulder_left_abduction_adduction(self, angles: list, target_state: AngleTargetStates, tolerance: int = 10):
+        return 0
