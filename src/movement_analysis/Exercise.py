fe4a188d9cd6daa62456c31fc67152c96aa6012b
DennisRitter <dennis.r.ritter@gmail.com>
2019-08-19 13:57:00 +0200

    moved result_state determination to extra private method

--- src/movement_analysis/Exercise.py
+++ src/movement_analysis/Exercise.py
@@ -27,30 +27,28 @@ class Exercise:
         }
         return result
 
-    def _check_angle_shoulder_left_flexion_extension(self, angle: float, target_state: AngleTargetStates, tolerance: int = 10) -> dict:
-        if target_state.value not in self.angles.keys():
-            warnings.warn("The target_state parameter value is not present in the Exercises' angles attribute. Cancelng Analysis.")
-            return []
-        
-        # Determine whether movement from START to END is Extension, Flexion, or None
-        target_end = self.angles[AngleTargetStates.END.value]["shoulder_left"]["flexion_extension"]["angle"][1]
-        target_start = self.angles[AngleTargetStates.START.value]["shoulder_left"]["flexion_extension"]["angle"][1]
+    def _get_angle_analysis_result_state(self,
+                                         angle: float,
+                                         target_state: AngleTargetStates,
+                                         target_start: float,
+                                         target_end: float,
+                                         target_min: float,
+                                         target_max: float,
+                                         tolerance: int) -> AngleAnalysisResultStates:
+
         target_end_is_flexion = target_end > target_start
         target_end_is_extension = target_end < target_start
         target_end_is_zero = target_end == target_start
 
-        target_min = min(self.angles[target_state.value]["shoulder_left"]["flexion_extension"]["angle"])
-        target_max = max(self.angles[target_state.value]["shoulder_left"]["flexion_extension"]["angle"])
-
         result_state = AngleAnalysisResultStates.NONE.value
         if target_state.value == AngleTargetStates.START.value:
             if angle < target_min - tolerance:
-                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_flexion else result_state
-                result_state = AngleAnalysisResultStates.TARGET_UNDERCUT.value if target_end_is_extension else result_state
-                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_zero else result_state
-            if angle > target_max + tolerance:
                 result_state = AngleAnalysisResultStates.TARGET_UNDERCUT.value if target_end_is_flexion else result_state
                 result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_extension else result_state
+                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_zero else result_state
+            if angle > target_max + tolerance:
+                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_flexion else result_state
+                result_state = AngleAnalysisResultStates.TARGET_UNDERCUT.value if target_end_is_extension else result_state
                 result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_zero else result_state
             if angle <= target_max + tolerance and angle >= target_min - tolerance:
                 result_state = AngleAnalysisResultStates.IN_TARGET_RANGE.value
@@ -67,60 +65,44 @@ class Exercise:
             if angle <= target_max + tolerance and angle >= target_min - tolerance:
                 result_state = AngleAnalysisResultStates.IN_TARGET_RANGE.value
 
+        return result_state
+
+    def _check_angle_shoulder_left_flexion_extension(self, angle: float, target_state: AngleTargetStates, tolerance: int = 10) -> dict:
+        if target_state.value not in self.angles.keys():
+            warnings.warn("The target_state parameter value is not present in the Exercises' angles attribute. Cancelng Analysis.")
+            return []
+
+        target_end = self.angles[AngleTargetStates.END.value]["shoulder_left"]["flexion_extension"]["angle"][1]
+        target_start = self.angles[AngleTargetStates.START.value]["shoulder_left"]["flexion_extension"]["angle"][1]
+        # Determine whether movement from START to END is Extension, Flexion, or None
+        target_min = min(self.angles[target_state.value]["shoulder_left"]["flexion_extension"]["angle"])
+        target_max = max(self.angles[target_state.value]["shoulder_left"]["flexion_extension"]["angle"])
+
         result = {
             "angle": angle,
             "target_min": target_min,
             "target_max": target_max,
             "target_state": target_state.value,
-            "result_state": result_state,
+            "result_state": self._get_angle_analysis_result_state(angle, target_state, target_start, target_end, target_min, target_max, tolerance),
         }
         return result
-    
+
     def _check_angle_shoulder_left_abduction_adduction(self, angle: float, target_state: AngleTargetStates, tolerance: int = 10):
         if target_state.value not in self.angles.keys():
             warnings.warn("The target_state parameter value is not present in the Exercises' angles attribute. Cancelng Analysis.")
             return []
-        
+
         # Determine whether movement from START to END is Extension, Flexion, or None
         target_end = self.angles[AngleTargetStates.END.value]["shoulder_left"]["abduction_adduction"]["angle"][1]
         target_start = self.angles[AngleTargetStates.START.value]["shoulder_left"]["abduction_adduction"]["angle"][1]
-        target_end_is_abd = target_end > target_start
-        target_end_is_add = target_end < target_start
-        target_end_is_zero = target_end == target_start
-
         target_min = min(self.angles[target_state.value]["shoulder_left"]["abduction_adduction"]["angle"])
         target_max = max(self.angles[target_state.value]["shoulder_left"]["abduction_adduction"]["angle"])
-
-        result_state = AngleAnalysisResultStates.NONE.value
-        if target_state.value == AngleTargetStates.START.value:
-            if angle < target_min - tolerance:
-                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_abd else result_state
-                result_state = AngleAnalysisResultStates.TARGET_UNDERCUT.value if target_end_is_add else result_state
-                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_zero else result_state
-            if angle > target_max + tolerance:
-                result_state = AngleAnalysisResultStates.TARGET_UNDERCUT.value if target_end_is_abd else result_state
-                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_add else result_state
-                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_zero else result_state
-            if angle <= target_max + tolerance and angle >= target_min - tolerance:
-                result_state = AngleAnalysisResultStates.IN_TARGET_RANGE.value
-
-        if target_state.value == AngleTargetStates.END.value:
-            if angle < target_min - tolerance:
-                result_state = AngleAnalysisResultStates.TARGET_UNDERCUT.value if target_end_is_abd else result_state
-                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_add else result_state
-                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_zero else result_state
-            if angle > target_max + tolerance:
-                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_abd else result_state
-                result_state = AngleAnalysisResultStates.TARGET_UNDERCUT.value if target_end_is_add else result_state
-                result_state = AngleAnalysisResultStates.TARGET_EXCEEDED.value if target_end_is_zero else result_state
-            if angle <= target_max + tolerance and angle >= target_min - tolerance:
-                result_state = AngleAnalysisResultStates.IN_TARGET_RANGE.value
 
         result = {
             "angle": angle,
             "target_min": target_min,
             "target_max": target_max,
             "target_state": target_state.value,
-            "result_state": result_state,
+            "result_state": self._get_angle_analysis_result_state(angle, target_state, target_start, target_end, target_min, target_max, tolerance),
         }
         return result
