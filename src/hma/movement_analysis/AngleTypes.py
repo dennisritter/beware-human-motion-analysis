f403b4d2b8ccdd436ae848eed030d7361c3585ea
DennisRitter <dennis.r.ritter@gmail.com>
2019-10-01 12:05:30 +0200

    created AngleTypes Enum with flexion, abduction and rotation types

--- src/hma/movement_analysis/AngleTypes.py
+++ src/hma/movement_analysis/AngleTypes.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 
 class AngleTypes(Enum):
-    FlexEx = 0  # Flexion / Extension
-    AbAd = 1  # Abduiction / Adduction
-    InExRot = 2  # Internal / External rotation
+    FLEX_EX = 0  # Flexion / Extension
+    AB_AD = 1  # Abduction / Adduction
+    IN_EX_ROT = 2  # Internal / External rotation
