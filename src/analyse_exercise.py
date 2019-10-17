153d3559d8342bc10aec602bc990c179e73a7d6a
DennisRitter <dennis.r.ritter@gmail.com>
2019-10-17 12:08:14 +0200

    research(Similarities): Edited scripts

--- src/analyse_exercise.py
+++ src/analyse_exercise.py
@@ -26,9 +26,11 @@ mocap_poseprocessor = PoseProcessor(PoseFormatEnum.MOCAP)
 # seq3 = mocap_poseprocessor.load('data/sequences/overheadpress-dennis-multi-3/complete-session.json', 'overheadpress-dennis-multi-3')
 # seqs = [seq1, seq2, seq3]
 
-g_seq = mocap_poseprocessor.load(
-    'data/sequences/unique_iterations/complete-session.json', 'squat-dennis-multi-1')
+# g_seq = mocap_poseprocessor.load('data/sequences/unique_iterations/complete-session.json', 'squat-dennis-multi-1')
+g_seq = mocap_poseprocessor.load('data/sequences/levente/overhead-press/multi/2019-09-25_15-09-03_records/complete-session.json', 'overheadpress')
 squat = exercise_loader.load('data/exercises/kniebeuge.json')
+overheadpress = exercise_loader.load('data/exercises/overhead-press.json')
+lungleleft = exercise_loader.load('data/exercises/lunge-left.json')
 EE = None
 
 seqs = []
@@ -49,7 +51,8 @@ for seq in seqs:
     else:
         merged_seq.merge(seq)
     if EE == None:
-        EE = ExerciseEvaluator(squat, merged_seq)
+        # EE = ExerciseEvaluator(squat, merged_seq)
+        EE = ExerciseEvaluator(overheadpress, merged_seq)
     else:
         EE.set_sequence(merged_seq)
     part_iterations_biased = EE.find_iteration_keypoints(20, 20)
