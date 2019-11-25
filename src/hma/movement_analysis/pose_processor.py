d914952455d48cb5b89539eb121ae7e627ca073c
DennisRitter <dennis.r.ritter@gmail.com>
2019-11-25 14:33:29 +0100

    pose_processor docstrings

--- src/hma/movement_analysis/pose_processor.py
+++ src/hma/movement_analysis/pose_processor.py
@@ -5,18 +5,23 @@ import numpy as np
 
 
 class PoseProcessor:
+    """Loads and Processes sequence file representations according to the defined poseformat.
+
+    Loads and Processes sequence file representations according to the defined poseformat. 
+    Each available poseformat needs a condition in the "self.process" function and a separate
+    "process_sequence_<poseformat>" function in order to load sequences of that poseformat.
+
+    Attributes:
+        poseformat (PoseFormatEnum): Specifies the pose format of sequences that will are imported. 
+    """
 
     def __init__(self, poseformat: PoseFormatEnum):
         """ PoseProcessor Constructor
-        Parameters
-        ----------
-        poseformat : PoseFormatEnum
-            A PoseFormat Enumeration member defining input format of the PoseProcessor instance.
+        Args:
+            poseformat (PoseFormatEnum): A PoseFormat Enumeration member defining input format of the PoseProcessor instance.
 
-        Returns
-        ----------
-        PoseProcessor
-            A PoseProcessor instance.
+        Returns:
+            (PoseProcessor) A PoseProcessor instance.
         """
         if(not isinstance(poseformat, PoseFormatEnum)):
             raise ValueError(
@@ -25,6 +30,13 @@ class PoseProcessor:
 
     def load(self, path: str, name: str = 'Some Sequence') -> Sequence:
         """ Loads a Sequence from path and maps it to the specified poseformat.
+
+        Args:
+            path (str): The path to a sequences JSON file.
+            name (str): Specifies the name of the returned sequence.
+
+        Returns:
+            (Sequence) A Sequence object.
         """
         with open(path, 'r') as myfile:
             seq = myfile.read()
@@ -32,55 +44,30 @@ class PoseProcessor:
 
     def process(self, input: str, name: str = 'Some Sequence') -> Sequence:
         """
-        Parameters
-        ----------
-        input : str
-            The motion sequence input string to convert.
+        Args:
+            input (str): The motion sequence input string to convert.
+            name (str): Specifies the name of the returned sequence.
         Returns
-        ----------
-        Sequence
-           The Sequence Object instance representing the motion sequence of the input string.
+            (Sequence): The Sequence Object instance representing the motion sequence of the input string.
         """
         if (self.poseformat == PoseFormatEnum.MOCAP):
             return self.process_sequence_mocap(input, name=name)
 
-    def process_sequence_mocap(self, input: str, name='sequence') -> Sequence:
+    def process_sequence_mocap(self, input: str, name='Some Sequence') -> Sequence:
         """
-        Parameters
-        ----------
-        input : str
-            The mocap motion sequence input string to convert.
+        Args
+            input (str): The MoCap motion sequence input string to convert.
+            name (str): Specifies the name of the returned sequence.
         Returns
-        ----------
-        Sequence
-           The Sequence Object instance representing the motion sequence of the input string.
+            (Sequence): The Sequence Object instance representing the MoCap motion sequence of the input string.
         """
-        MOCAP_BODY_PARTS = {
-            "LeftWrist": 0,
-            "LeftElbow": 1,
-            "LeftShoulder": 2,
-            "Neck": 3,
-            "Torso": 4,
-            "Waist": 5,
-            "LeftAnkle": 6,
-            "LeftKnee": 7,
-            "LeftHip": 8,
-            "RightAnkle": 9,
-            "RightKnee": 10,
-            "RightHip": 11,
-            "RightWrist": 12,
-            "RightElbow": 13,
-            "RightShoulder": 14,
-            "Head": 15
-        }
         mocap_sequence = json.loads(input)
-
         body_parts = mocap_sequence["format"]
         positions = np.array(mocap_sequence["frames"])
         timestamps = np.array(mocap_sequence["timestamps"])
 
         # reshape positions to 3d array
-        positions = np.reshape(positions, (np.shape(positions)[0], int(np.shape(positions)[1]/3), 3))
+        positions = np.reshape(positions, (np.shape(positions)[0], int(np.shape(positions)[1] / 3), 3))
 
         # Center Positions by subtracting the mean of each coordinate
         positions[:, :,
