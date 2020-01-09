39cc5b7c4560c600f810b5fb7b582e9570a309d9
Dennis Ritter <dennis.r.ritter@gmail.com>
2020-01-09 16:52:51 +0100

    feat: Plotting skeleton with shoulder and hip JCS

--- src/plotly-testing.py
+++ src/plotly-testing.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import numpy as np
 from hma.movement_analysis.pose_processor import PoseProcessor
 from hma.movement_analysis.enums.pose_format_enum import PoseFormatEnum
+from hma.movement_analysis import transformations
 
 mocap_poseprocessor = PoseProcessor(PoseFormatEnum.MOCAP)
 filename = "data/sequences/191024_tracking/single/squat/user-2/191024__single__squat__user-2__1.json"
@@ -25,7 +26,7 @@ scene = dict(
         range=[-1000, 1000],
     ),
     camera=dict(
-        up=dict(x=0,y=1,z=0),
+        up=dict(x=0, y=1, z=0),
         eye=dict(x=-1.5, y=1.5, z=-1.5)
     )
 )
@@ -34,11 +35,50 @@ layout = go.Layout(
     scene=scene,
 )
 
-def get_traces(): 
+def get_lcs_trace(origin, x_direction_pos, y_direction_pos):
+    """ Returns a list that contains a plotly trace object the X, Y and Z axes of the local joint coordinate system calculated from and origin, a X-axis-direction and a Y-axis-direction.
+    """
+
+    # Get Local Coordinate System vectors
+    lcs = transformations.get_local_coordinate_system_direction_vectors(
+        origin,
+        x_direction_pos,
+        y_direction_pos
+    )
+
+    # Set Local Coordinate System vectors' length to 100 and move relative to local origin
+    lcs[0] = lcs[0] * 100 + origin
+    lcs[1] = lcs[1] * 100 + origin
+    lcs[2] = lcs[2] * 100 + origin
+    trace_x = go.Scatter3d(
+        x=[origin[0], lcs[0, 0]],
+        y=[origin[1], lcs[0, 1]],
+        z=[origin[2], lcs[0, 2]],
+        mode="lines",
+        marker=dict(color="red")
+    )
+    trace_y = go.Scatter3d(
+        x=[origin[0], lcs[1, 0]],
+        y=[origin[1], lcs[1, 1]],
+        z=[origin[2], lcs[1, 2]],
+        mode="lines",
+        marker=dict(color="green")
+    )
+    trace_z = go.Scatter3d(
+        x=[origin[0], lcs[2, 0]],
+        y=[origin[1], lcs[2, 1]],
+        z=[origin[2], lcs[2, 2]],
+        mode="lines",
+        marker=dict(color="blue")
+    )
+    return [trace_x, trace_y, trace_z]
+
+
+def get_traces():
     trace_joints = go.Scatter3d(
-        x=p[frame,:, 0],
-        y=p[frame,:, 1],
-        z=p[frame,:, 2],
+        x=p[frame, :, 0],
+        y=p[frame, :, 1],
+        z=p[frame, :, 2],
         mode="markers",
         marker=dict(color="royalblue"))
     trace_lelbow_lwrist = go.Scatter3d(
@@ -148,6 +188,13 @@ def get_traces():
         line=dict(color="firebrick", width=5)
     )
 
+    # Get Local Joint Coordinate System Axis traces 
+    bps = sequence.body_parts
+    ls_lcs_traces = get_lcs_trace(p[frame, bps["LeftShoulder"]], p[frame, bps["RightShoulder"]], p[frame, bps["Torso"]])
+    rs_lcs_traces = get_lcs_trace(p[frame, bps["RightShoulder"]], p[frame, bps["LeftShoulder"]], p[frame, bps["Torso"]])
+    lh_lcs_traces = get_lcs_trace(p[frame, bps["LeftHip"]], p[frame, bps["RightHip"]], p[frame, bps["Torso"]])
+    rh_lcs_traces = get_lcs_trace(p[frame, bps["RightHip"]], p[frame, bps["LeftHip"]], p[frame, bps["Torso"]])
+    
     traces = [
         trace_joints,
         trace_head_neck,
@@ -164,7 +211,19 @@ def get_traces():
         trace_lknee_lankle,
         trace_waist_rhip,
         trace_rhip_rknee,
-        trace_rknee_rankle
+        trace_rknee_rankle,
+        ls_lcs_traces[0],
+        ls_lcs_traces[1],
+        ls_lcs_traces[2],
+        rs_lcs_traces[0],
+        rs_lcs_traces[1],
+        rs_lcs_traces[2],
+        lh_lcs_traces[0],
+        lh_lcs_traces[1],
+        lh_lcs_traces[2],
+        rh_lcs_traces[0],
+        rh_lcs_traces[1],
+        rh_lcs_traces[2]
     ]
 
     return traces
