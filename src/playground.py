3c137cced8873b676bb45b41e7b559e9265aa2ee
Dennis Ritter <dritter@beuth-hochschule.de>
2020-02-20 10:57:34 +0100

    chore(script): scripting test

--- src/playground.py
+++ src/playground.py
@@ -21,12 +21,12 @@ import plotly.graph_objects as go
 # exercise = "data/exercises/squat.json"
 exercise = "data/exercises/overhead-press.json"
 # filename = "data/sequences/191024_tracking/single/squat/user-2/191024__single__squat__user-2__1.json"
-# filename = "data/sequences/191024_tracking/single/squat/user-6/191024__single__squat__user-6__1.json"
+# filename = "data/sequences/191024_tracking/single/squat/user-4/191024__single__squat__user-4__1.json"
 # filename = "data/sequences/191024_tracking/single/biceps_curl_left/user-2/191024__single__biceps_curl_left__user-2__1.json"
 # filename = "data/sequences/191024_tracking/single/biceps_curl_right/user-2/191024__single__biceps_curl_right__user-2__1.json"
 # filename = "data/sequences/191024_tracking/single/triceps_extension_left/user-2/191024__single__triceps_extension_left__user-2__1.json"
 # filename = "data/sequences/191024_tracking/single/triceps_extension_right/user-2/191024__single__triceps_extension_right__user-2__1.json"
-filename = "data/sequences/191024_tracking/single/overhead_press/user-2/191024__single__overhead_press__user-2__1.json"
+filename = "data/sequences/191024_tracking/single/overhead_press/user-3/191024__single__overhead_press__user-3__6.json"
 # filename = "data/sequences/191024_tracking/single/overhead_press/user-5/191024__single__overhead_press__user-5__10.json"
 # filename = "data/sequences/191024_tracking/single/lunge_left/user-2/191024__single__lunge_left__user-2__1.json"
 # filename = "data/sequences/test/100_frames.json"
@@ -50,9 +50,9 @@ fig.add_trace(go.Scatter(x=np.arange(len(s)), y=s.joint_angles[:, bps['shoulder_
 
 fig.show()
 
-s_sv = s[::10]
-sv = SkeletonVisualiser(s_sv)
-sv.show()
+# s_sv = s[::10]
+# sv = SkeletonVisualiser(s_sv)
+# sv.show()
 
 exe = Exercise.from_file(exercise)
 EE = ExerciseEvaluator(exe, s)
